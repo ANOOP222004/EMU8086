8849bdf6a92f34302f8db018816daa152ca5c875
ANOOP222004 <anoop22122004@gmail.com>
2024-06-05 22:04:14 +0530

    Update move block of 10 8bit numbers.asm

--- move block of 10 8bit numbers.asm
+++ move block of 10 8bit numbers.asm
@@ -3,14 +3,17 @@
 ; The location of this template is c:\emu8086\inc\0_com_template.txt
 
 org 100h
-mov cl,10
-mov si,1000h
-mov di,1050h
+mov cl,10 ;counter
+mov si,1000h ;mov to source index
+mov di,1050h ;mov to destination index
 back:
-mov al,[si]
+;si and dl are swaped in this and next step
+mov al,[si] 
 mov [di],al
-inc si
+; point to next number
+inc si 
 inc di
+;decrement count untill zero
 dec cl
 jnz back
 nop
