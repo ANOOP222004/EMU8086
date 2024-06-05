671b875d79778469c8eaeef67cea0698897d9bae
ANOOP222004 <anoop22122004@gmail.com>
2024-06-05 22:14:39 +0530

    Update Ascending order of numbers.asm

--- Ascending order of numbers.asm
+++ Ascending order of numbers.asm
@@ -3,17 +3,18 @@
 ; The location of this template is c:\emu8086\inc\0_com_template.txt
 
 org 100h
-mov cl,10
-mov si,1000h
+mov cl,10 ;assign counter
+mov si,1000h ;assign source index
 back:
-mov al,[si]
-cmp al,[si+1]
+mov al,[si] ;assign al as 1st number
+cmp al,[si+1] ;compare 1st and 2nd number
 js noexchange
+;exchange 1st and 2nd number
 xchg al,[si+1]
 xchg al,[si]
 noexchange:
-inc si
-dec cl
+inc si ;point next number
+dec cl ;decrement counter 
 jnz back
 ret
 
