c03c88f06e7014fb2689ba75241c506c8eb53e42
ANOOP222004 <anoop22122004@gmail.com>
2024-06-05 22:07:24 +0530

    Update move block of 10 16bit numbers.asm

--- move block of 10 16bit numbers.asm
+++ move block of 10 16bit numbers.asm
@@ -2,13 +2,15 @@
 ; You may customize this and other start-up templates; 
 ; The location of this template is c:\emu8086\inc\0_com_template.txt
 
-org 100h
-mov cl,10
-mov si,1000h
-mov di,1050h
+org 100h 
+mov cl,10 ;counter
+mov si,1000h ;move to source index
+mov di,1050h ;move to destination index
 back:
+;copy si to di
 mov ax,[si]
 mov [di],ax
+;
 inc si
 inc di
 dec cl
