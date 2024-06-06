4b2cee43f738cc24742d5c1f4361b39036d0d1fb
Anoop B A <anoop22122004@gmail.com>
2024-06-06 22:59:56 +0530

    Update largest of 3 numbers.asm

--- largest of 3 numbers.asm
+++ largest of 3 numbers.asm
@@ -1,14 +1,14 @@
 org 100h
-mov al,3
-mov bl,2
-mov cl,1
-cmp al,bl
+mov al,3 ;assign a
+mov bl,2 ;assign b
+mov cl,1 ;assign c
+cmp al,bl ;compare a and b
 jg label1
-cmp bl,cl
+cmp bl,cl ;compare b and c
 jg lb
 jmp lc
 label1:
-cmp al,cl
+cmp al,cl ;compare a nad c
 jg la
 
 lc:
